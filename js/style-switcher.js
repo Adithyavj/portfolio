ca4a8770d06ea070386ca28c7cf584e9c94c4ce9
Adithya Vijay K <adithyavj@gmail.com>
2021-05-07 20:52:42 +0530

    dark mode

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -11,3 +11,32 @@ window.addEventListener("scroll",()=>{
         document.querySelector(".style-switcher").classList.remove("open")
     }
 })
+
+// theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style)=>{
+        if(color==style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled",true);
+        }
+    })
+}
+
+// theme light and dark mode
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click",()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load",()=>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
